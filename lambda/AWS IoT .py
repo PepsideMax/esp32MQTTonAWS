0762a44f62b7d48991e14dea48c9608f9f88531a
PepsideMax <38300052+PepsideMax@users.noreply.github.com>
2021-03-20 13:55:50 +0100

    Update AWS IoT .py

--- lambda/AWS IoT .py
+++ lambda/AWS IoT .py
@@ -4,7 +4,7 @@ import boto3
 led_topic = "<YOUR_LED_TOPIC>"
 temp = 22.00
 
-client = boto3.client('iot-data', region_name='eu-west-1')
+client = boto3.client('iot-data', region_name='us-west-2')
 
 def lambda_handler(event, context):
     if float(event["temperature"]) > temp:
